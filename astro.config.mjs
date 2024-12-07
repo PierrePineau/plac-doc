57fc9d6e271f7485e8db9c7157d352c16f85f146
Pierre Pineau <pierrepineau.pro@gmail.com>
2024-12-07 16:04:25 +0100

    Documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,49 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Plac Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/PierrePineau/plac-doc',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Installation',
+					autogenerate: { directory: 'installation' },
+					badge: { text: 'DEV', variant: 'note' }
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+                {
+					label: 'Plac API',
+                    collapsed: true,
+					autogenerate: { directory: 'api' },
+					badge: { text: 'DEV', variant: 'note' }
 				},
+                {
+					label: 'Plac App',
+                    collapsed: true,
+					autogenerate: { directory: 'app' },
+					badge: { text: 'DEV', variant: 'note' }
+				},
+				// {
+				// 	label: 'Guides Utilisateurs',
+				// 	badge: { text: 'User', variant: 'caution' },
+                //     collapsed: true,
+                //     items: [
+                //         {
+                //             label: 'Introduction',
+                //             link: '/guides/',
+                //         },
+                //         {
+                //             label: 'Interface Admin',
+                //             collapsed: true,
+                //             autogenerate: { directory: 'guides/admin' }
+                //         },
+                //         {
+                //             label: 'Interface App',
+                //             collapsed: true,
+                //             autogenerate: { directory: 'guides/app' },
+                //         },
+                //     ]
+				// },
 			],
 		}),
 	],
